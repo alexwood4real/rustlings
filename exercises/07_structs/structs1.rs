799e677bc0fa227a69a1f05f4e50aa9bb54f4738
alexwood4real <akwood6@asu.edu>
2026-05-24 13:32:50 -0700

    Finished Structs
    this one was very interesting. there are three kinds of structs:
    - regular c-like structs that work the same
    - tuple structs which dont have argument names but you access the index
    -  unit structs which just create new structs with nothing defined yet
    
    you can create structs but doing = StructName = { args }
    
    in order to create methods for a struct, you put them inside imple StructName{ } and instead of calling the type, you reference self
    
    - the ::new() is a constructor that creates a new instance when one doesnt exist prior. this was confusing because i had to change a function name

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,10 +1,27 @@
+/* C-like struct and its constuctor */
 struct ColorRegularStruct {
     // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: u32,
+    green: u32,
+    blue: u32
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+fn init_color( red: u32, blue: u32, green: u32 ) -> ColorRegularStruct
+    {
+    ColorRegularStruct
+        {
+        /* can do attribute: arg, but since they have the same name it is ok */
+        red,
+        green,
+        blue
+        }
+    }
 
+/* tuple struct */
+struct ColorTupleStruct( u32, u32, u32 );
+
+/* unit struct */
 #[derive(Debug)]
 struct UnitStruct;
 
@@ -19,17 +36,27 @@ mod tests {
     #[test]
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
-        // let green =
+        let green = ColorRegularStruct {
+            green: 255,
+            red: 0,
+            blue: 0
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
         assert_eq!(green.blue, 0);
+
+        let blue = init_color( 0, 255, 0 );
+
+        assert_eq!(blue.red, 0);
+        assert_eq!(blue.green, 0);
+        assert_eq!(blue.blue, 255);
     }
 
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
-        // let green =
+        let green = ( 0, 255, 0 );
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -39,7 +66,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
