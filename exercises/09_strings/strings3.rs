6a0f42d788ac1276679bac25a896a968c5e0bccc
alexwood4real <akwood6@asu.edu>
2026-06-23 05:56:09 -0700

    Finished Strings
    This one was tough. I will have to refer back to this, but String and &str are different in rust

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,22 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    let trime_ws: &[char] = &[ ' ' ];
+    let trimmed_str: &str = input.trim_matches( trime_ws );
+    trimmed_str
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut world_str: String  = String::new();
+    world_str.push_str( input );
+    world_str.push_str( " world!");
+    world_str
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let replaced: String = input.replace( "cars", "balloons" );
+    replaced
 }
 
 fn main() {
