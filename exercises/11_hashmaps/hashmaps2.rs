27526b4a1f1d5558212e4cf6bae03daac9c602c0
alexwood4real <akwood6@asu.edu>
2026-06-26 06:46:56 -0700

    Finished HashMaps
    I don't have a lot of experience with maps so this was interesting and lots to remember
    - we can insert linearly with <map>.insert( <key>, <val> )
    - we can get the values by getting a copy and unwapping the key
    - we dont even have to check, we can use .or_default to insert a key that isnt there

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -32,6 +32,15 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        let quant = basket.get( &fruit ).copied().unwrap_or( 1 );
+        if quant == 0
+            {
+            basket.insert( fruit, quant );
+            }
+        else
+            {
+            basket.entry( fruit ).or_insert( quant );
+            }
     }
 }
 
